71bcf0e4cac9960a5e9a5b905f8c37a1594544f5
Allenhuh <53265298+Allenhuh@users.noreply.github.com>
2021-04-04 19:11:01 +0800

    Update App.js
    Test edit description

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hi Welcome to POTL!!!
+        </p>
+        <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
