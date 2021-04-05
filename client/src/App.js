66550435d0298e5a38418f3100cddde359be8163
allenhuh <under-the-ocean@hotmail.com>
2021-04-05 22:57:23 +0800

    test edit

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi, Welcome to POTL!
+          Hello, Welcome to POTL!
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
